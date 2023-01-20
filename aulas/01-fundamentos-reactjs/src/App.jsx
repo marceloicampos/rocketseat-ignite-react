e9c4a743ef11249541dda6374de6ed05bfd71e46
marceloicampos <marcelo.ildefonso@hotmail.com>
2023-01-20 13:40:14 -0300

    using normal css import and add Header Component

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,14 +1,23 @@
+import { Header } from './components/Header'
 import { Post } from './components/Post'
+import './styles.css'
+// acima estamos importando estilos sem o CSS Module
 
 export default function App() {
     return (
-        <>
-            <h1>Hello World !</h1>
-            <Post author={'Marcelo Campos'} content={'Post Today'}/>
-            <Post author={'Duda Campos'} content={'Post Tomorrow'}/>
-            <Post author={'Caio Campos'} content={'Post Yesterday'}/>
-        </>
+        <div>
+            <Header />
+            <Post author={'Marcelo Campos'} content={'Post Today'} />
+            <Post author={'Duda Campos'} content={'Post Tomorrow'} />
+            <Post author={'Caio Campos'} content={'Post Yesterday'} />
+        </div>
     )
 }
 
+// function App () {
+//    return (
+//
+//      )
+// }
 // export default App
+// acima temos a exportação padrão de um componente
